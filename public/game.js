03bffae2b8516aeacd104368f70f914f094b6301
rlftrifork <rlf@trifork.com>
2022-05-02 13:19:55 +0200

    Nice

--- public/game.js
+++ public/game.js
@@ -1,3 +1,30 @@
-const grid = document.querySelector(".grid")
+const gridElement = document.querySelector(".grid-container")
+const gridSize = 10
 
-console.log("Det virker nu")
+let grid = [];
+
+function createGrid(){
+    for (let i = 0; i < gridSize; i++) {
+        grid[i] = []
+        for (let j = 0; j < gridSize; j++) {
+            grid[i][j] = "wall"
+        }
+    }
+}
+createGrid()
+console.table(grid)
+console.log(gridElement)
+
+grid.forEach((row, x) =>{
+
+    row.forEach((cell, y)=>{
+        console.log(x,y, row, cell)
+        let newCell = document.createElement('div')
+        newCell.classList.add("grid-item")
+        newCell.classList.add("wall")
+        newCell.innerHTML = cell
+
+        gridElement.appendChild(newCell)
+    })
+})
+
