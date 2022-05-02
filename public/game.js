9a169198fc18e024acb761fa50bf783af7dc2d5a
rlftrifork <rlf@trifork.com>
2022-05-02 15:39:39 +0200

    Nice

--- public/game.js
+++ public/game.js
@@ -1,33 +1,18 @@
-const gridElement = document.querySelector(".grid-container")
-const gridSize = 10
+const gameState = {}
 
-let grid = [];
+const socket = io();
 
-let cells = Object.freeze([{name:"wall", img:"bricks.png" }])
+const dice = document.querySelector(".dice")
 
-function createGrid(){
-    for (let i = 0; i < gridSize; i++) {
-        grid[i] = []
-        for (let j = 0; j < gridSize; j++) {
-            grid[i][j] = "wall"
-        }
-    }
-}
-createGrid()
-console.table(grid)
-console.log(gridElement)
 
-grid.forEach((row, x) =>{
-
-    row.forEach((cell, y)=>{
-        // console.log(x,y, row, cell)
-        let newCell = new Image(100,100)
-        newCell.src = 'bricks.js'
-        newCell.classList.add("grid-item")
-        newCell.classList.add("wall")
-        newCell.innerHTML = cell
-
-        gridElement.appendChild(newCell)
+function roll(){
+    let fisk = 0
+    socket.emit('takeTurn', "roll")
+    socket.on("roll", e=>{
+        fisk += 1
+        console.log(fisk)
+        console.log(e)
+        // dice.innerHTML = e
     })
-})
+}
 
