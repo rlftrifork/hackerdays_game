e9c7cec237dc4cc5cbee7578d4e0cfeadd930d37
rlftrifork <rlf@trifork.com>
2022-05-02 12:41:24 +0200

    grid

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.static('public'))
+
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 
 io.on('connection', (socket) => {
@@ -21,6 +23,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(5000, () => {
+    console.log('listening on *:5000');
 });
---
+++ public/game.js
@@ -0,0 +1,3 @@
+const grid = document.querySelector(".grid")
+
+console.log("Det virker nu")
